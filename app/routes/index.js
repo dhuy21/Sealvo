c4ffbccf5935bcc0819d0ef130116673a2e8ed48
dhuy21 <huynguyen2192004@gmail.com>
2025-04-09 00:24:24 +0200

    Implement user authentication routes and add my vocabulary dashboard

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,12 +2,14 @@ const newUserLogin = require('./user/login');
 const newUserRegister = require('./user/registre');
 const newUserLogout = require('./user/logout');
 const siteRouter = require('./site');
-
+const newVocabsRouter = require('./vocab/monVocabs');
 
 function route(app){
     app.use('/login', newUserLogin);
     app.use('/registre', newUserRegister);
     app.use('/logout', newUserLogout);
+    app.use('/monVocabs', newVocabsRouter);
+    // Utiliser le router site pour toutes les routes, y compris /aboutme
     app.use('/', siteRouter);
 }
 
--- app/routes/user/registre.js
+++ app/routes/user/registre.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../../controllers/UserController');
 
-// Route pour la page de connexion
+// Route pour afficher la page d'inscription
 router.get('/', userController.registre);
+
+// Route pour traiter la soumission du formulaire d'inscription
+router.post('/', userController.registrePost);
 
 module.exports = router;
