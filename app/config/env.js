e1e6b0ba81d15552fa49cb6e27623ca1accf406b
dhuy21 <huynguyen2192004@gmail.com>
2025-04-27 18:01:03 +0200

    Refactor database and server configuration to use centralized config file for improved maintainability

---
+++ app/config/env.js
@@ -0,0 +1,16 @@
+module.exports = {
+    // Database Configuration
+    DB_HOST: 'hopper.proxy.rlwy.net',
+    DB_USER: 'root',
+    DB_PASSWORD: 'TTuanfuExeJILPWmVdseSgGfnjkWixyW',
+    DB_DATABASE: 'web_db',
+    DB_PORT: 15400,
+
+    // Server Configuration
+    PORT: 8080,
+
+    
+
+    // Application Environment
+    NODE_ENV: 'development'
+}; 
