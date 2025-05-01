b11d66237e2de300ba6e1a4acd20788118de4501
dhuy21 <huynguyen2192004@gmail.com>
2025-05-02 01:40:50 +0200

    feat: Add DigiCert Global Root Certificate and update database configuration

--- app/config/env.js
+++ app/config/env.js
@@ -1,10 +1,11 @@
 module.exports = {
     // Database Configuration
-    DB_HOST: 'localhost',
-    DB_USER: 'root',
-    DB_PASSWORD: '123456789',
+    DB_HOST: 'web-server.mysql.database.azure.com',
+    DB_USER: 'dnguye06',
+    DB_PASSWORD: '@ComtamSUON##21',
     DB_DATABASE: 'web_db',
-    DB_PORT: 15400,
+    DB_PORT: 3306,
+    DB_DIALECT: 'mysql',
 
     // Server Configuration
     PORT: 8080,
