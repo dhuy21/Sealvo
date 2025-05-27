ed3da1e7f5f656fee6c5da2c75b6c023434f4491
dhuy21 <dinh_huy.nguyen@insa-cvl.fr>
2025-05-28 01:06:57 +0200

    feat: Add pronunciation test game and update routing for streak API

---
+++ app/routes/game/test_pronun.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const testPronunController = require('../../controllers/gameControllers/TestPronunController');
+
+// Middleware d'authentification
+const isAuthenticated = (req, res, next) => {
+    if (req.session.user) {
+        return next();
+    }
+    res.redirect('/login?error=Vous devez être connecté pour accéder à cette page');
+};
+
+//Route pour récupérer les mots pour le jeu
+router.get('/word', isAuthenticated, testPronunController.getWordForTestPronun);
+
+module.exports = router;
