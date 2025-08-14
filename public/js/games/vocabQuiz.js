a4e5f3a0679254d56cb1775ae8d8195a285a12fc
dhuy21 <dinh_huy.nguyen@insa-cvl.fr>
2025-08-14 16:49:04 +0200

    refactor: Revamp confetti animation logic across multiple games to enhance visual effects; streamline particle generation and improve performance for a more engaging user experience

--- public/js/games/vocabQuiz.js
+++ public/js/games/vocabQuiz.js
@@ -661,37 +661,41 @@ document.addEventListener('DOMContentLoaded', function() {
     
     // Fonction pour lancer l'animation confetti avec confetti.js.org
     function launchConfetti() {
-        const duration = 15* 1000;
-        const animationEnd = Date.now() + duration;
-        const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+        const duration = 15 * 1000,
+        animationEnd = Date.now() + duration;
+
+        let skew = 1;
 
         function randomInRange(min, max) {
-            return Math.random() * (max - min) + min;
+        return Math.random() * (max - min) + min;
         }
 
-        const interval = setInterval(function() {
-            const timeLeft = animationEnd - Date.now();
-          
-            if (timeLeft <= 0) {
-              return clearInterval(interval);
-            }
-          
-            const particleCount = 50 * (timeLeft / duration);
-          
-            // since particles fall down, start a bit higher than random
-            confetti(
-              Object.assign({}, defaults, {
-                particleCount,
-                origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
-              })
-            );
-            confetti(
-              Object.assign({}, defaults, {
-                particleCount,
-                origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
-              })
-            );
-          }, 250);
+        (function frame() {
+        const timeLeft = animationEnd - Date.now(),
+            ticks = Math.max(200, 500 * (timeLeft / duration));
+
+        skew = Math.max(0.8, skew - 0.001);
+
+        confetti({
+            particleCount: 1,
+            startVelocity: 0,
+            ticks: ticks,
+            origin: {
+            x: Math.random(),
+            // since particles fall down, skew start toward the top
+            y: Math.random() * skew - 0.2,
+            },
+            colors: ["#ffffff"],
+            shapes: ["circle"],
+            gravity: randomInRange(0.4, 0.6),
+            scalar: randomInRange(0.4, 1),
+            drift: randomInRange(-0.4, 0.4),
+        });
+
+        if (timeLeft > 0) {
+            requestAnimationFrame(frame);
+        }
+        })();
     }
 
     // Événements
